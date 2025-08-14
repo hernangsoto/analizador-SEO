31bfddd9680de02b717778600c7bf9b5c32b858a
hernangsoto <hernangsoto@gmail.com>
2025-08-14 02:30:22 -0300

    utils.py

--- modules/utils.py
+++ modules/utils.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import json
+
+def debug_log(msg: str, data=None):
+    if st.session_state.get("DEBUG"):
+        st.info(msg)
+        if data is not None:
+            try:
+                st.code(json.dumps(data, indent=2, ensure_ascii=False))
+            except Exception:
+                st.code(str(data))
 
